883cda70424675be074eee0370ed0cc3b2e73f08
Anubhab Rakshit <95529010+Anubhab-Rakshit@users.noreply.github.com>
2025-06-21 21:48:14 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import HeroSection from "@/components/hero-section"
 import CardsShowcase from "@/components/cards-showcase"
 import AboutSection from "@/components/about-page"
 import OpportunitiesSection from "@/components/opportunities-section"
+import BenefitsSection from "./about-us/components/benefits-section"
+import InternshipJourneySection from "./about-us/components/internship-journey-section"
+
 export default function HomePage() {
   return (
     <div className="bg-gradient-to-br from-gray-900 via-black to-gray-800">
@@ -11,6 +14,8 @@ export default function HomePage() {
         <HeroSection />
         <OpportunitiesSection />
         <CardsShowcase />
-    </div>
+        <BenefitsSection />
+       <InternshipJourneySection />
+    </div>  
   )
 }
